44b1de918ee903d35212de0c8a9212b6bbf95264
esoemad5 <esoemad5@gmail.com>
2018-10-25 08:02:57 -0500

    Cleaned up comments

--- SuperheroCreator/SuperheroCreator/Controllers/SuperheroesController.cs
+++ SuperheroCreator/SuperheroCreator/Controllers/SuperheroesController.cs
@@ -14,34 +14,23 @@ namespace SuperheroCreator.Controllers
         {
             db = new ApplicationDbContext();
         }
-        // GET: Superheroes
+
         public ActionResult Index()
         {
             return View();
         }
-
         
-
-        // GET: Superheroes/Create
+    
         public ActionResult Create()
         {
             return View(new Superhero());
         }
-
-        // POST: Superheroes/Create
+        
         [HttpPost]
         public ActionResult Create([Bind(Include ="Name,AlterEgo,PrimaryAbility,SecondaryAbility,Catchphrase")] Superhero hero)
         {
             try
             {
-                //Superhero newSuperhero = new Superhero();
-                //newSuperhero.Name = collection.ToValueProvider().GetValue("Name").AttemptedValue;
-                //newSuperhero.AlterEgo = collection.ToValueProvider().GetValue("AlterEgo").AttemptedValue;
-                //newSuperhero.PrimaryAbility = collection.ToValueProvider().GetValue("PrimaryAbility").AttemptedValue;
-                //newSuperhero.SecondaryAbility = collection.ToValueProvider().GetValue("SecondaryAbility").AttemptedValue;
-                //newSuperhero.Catchphrase = collection.ToValueProvider().GetValue("Catchphrase").AttemptedValue;
-
-                //db.Superheroes.Add(newSuperhero);
                 db.Superheroes.Add(hero);
                 db.SaveChanges();
                 // The hero object gets a new ID when it is put into the database. Prior to that, it is set to default 0.
@@ -57,20 +46,17 @@ namespace SuperheroCreator.Controllers
         {
             return View(db.Superheroes.ToList());
         }
-
-        // GET: Superheroes/Details/5
+        
         public ActionResult Read(int id)
         {
             return View(db.Superheroes.Where(s => s.ID == id).Single());
         }
-
-        // GET: Superheroes/Edit/5
+        
         public ActionResult Update(int id)
         {
             return View(db.Superheroes.Where(s => s.ID == id).Single());
         }
-
-        // POST: Superheroes/Edit/5
+        
         [HttpPost]
         public ActionResult Update(int id, [Bind(Include = "Name,AlterEgo,PrimaryAbility,SecondaryAbility,Catchphrase")] Superhero hero)
         {
@@ -78,17 +64,8 @@ namespace SuperheroCreator.Controllers
             {
                 // The hero's ID changes with this method
                 db.Superheroes.Remove(db.Superheroes.Where(s => s.ID == id).Single());
-                
                 db.Superheroes.Add(hero);
-
-                //db.Superheroes.Where(s => s.ID == id).Single().Name = hero.Name;
-                //db.Superheroes.Where(s => s.ID == id).Single().AlterEgo = hero.AlterEgo;
-                //db.Superheroes.Where(s => s.ID == id).Single().PrimaryAbility = hero.PrimaryAbility;
-                //db.Superheroes.Where(s => s.ID == id).Single().SecondaryAbility = hero.SecondaryAbility;
-                //db.Superheroes.Where(s => s.ID == id).Single().Catchphrase = hero.Catchphrase;
-
                 db.SaveChanges();
-
                 return RedirectToAction("List");
             }
             catch
@@ -96,14 +73,12 @@ namespace SuperheroCreator.Controllers
                 return View();
             }
         }
-
-        // GET: Superheroes/Delete/5
+        
         public ActionResult Delete(int id)
         {
             return View(db.Superheroes.Where(s => s.ID == id).Single());
         }
 
-        // POST: Superheroes/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
